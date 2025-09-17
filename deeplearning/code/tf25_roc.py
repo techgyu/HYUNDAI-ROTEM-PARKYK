eea6c33b7ba188724b50f32c30069c61ae4d7b71
ubuntu_gyuteak <rlarbxor85@gmail.com>
2025-09-17 14:50:41 +0900

    ubuntu에서 windows 전환 전

--- deeplearning/code/tf25_roc.py
+++ deeplearning/code/tf25_roc.py
@@ -37,8 +37,9 @@ print(x_scale[:2])
 
 x_train, x_test, y_train, y_test = train_test_split(x_scale, y, test_size=0.3)
 
-n_features = x_train[1]
-n_classes = y_train[1]
+# shape에서 int로 추출해야 함
+n_features = x_train.shape[1]
+n_classes = y_train.shape[1]
 print(n_features, n_classes)
 
 from tensorflow.keras.models import Sequential
@@ -52,7 +53,64 @@ def create_model_func(input_dim, out_dim, out_nodes, n, model_name = 'model'):
         for _ in range(n):
             model.add(Dense(10, activation='relu'))
         model.add(Dense(out_dim, activation='softmax'))
-        
+
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
         return model
-[create_model_func(n_features, n_classes, 10, n, 'model_{}'.format(n)) for n in range(1, 4)]
+    return create_model  # 이 줄 추가!
+models = [create_model_func(n_features, n_classes, 10, n, 'model_{}'.format(n)) for n in range(1, 4)]
+print(len(models))
+
+for create_model in models:
+    print()
+    create_model().summary()
+
+history_dict = {}
+for create_model in models:
+    model = create_model()
+    print('모델명 :', model.name)
+    history = model.fit(x_train, y_train, batch_size=8, epochs=50, verbose=0, validation_split=0.2)
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print('test dataset loss: ', score[0])
+    print('test dataset accuracy: ', score[1])
+    history_dict[model.name] = [history, model]
+
+print(history_dict)
+
+
+# 시각화
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 7))
+
+for model_name in history_dict:
+    print('h_d : ', history_dict[model_name][0].history['accuracy'])
+    val_acc = history_dict[model_name][0].history['val_accuracy']
+    val_loss = history_dict[model_name][0].history['val_loss']
+    ax1.plot(val_acc, label=model_name)
+    ax2.plot(val_loss, label=model_name)
+    ax1.set_ylabel('val_accuracy')
+    ax2.set_ylabel('val_loss')
+    ax2.set_xlabel('epoch')
+    ax1.legend()
+    ax2.legend()
+plt.savefig('./deeplearning/data/user_data/images/25_roc_1.png')
+
+# ROC Curve : 분류기에 대한 성능 평가 방벙중 하나
+from sklearn.metrics import roc_curve, auc
+
+
+plt.figure()
+plt.plot([0, 1], [0,1], 'k--')
+
+for model_name in history_dict:
+    model = history_dict[model_name][1]
+    y_pred = model.predict(x_test)
+    # Convert sparse matrices to dense arrays and then ravel
+    fpr, tpr, _ = roc_curve(y_test.ravel(), y_pred.ravel())
+    plt.plot(fpr, tpr, label = '{}, auc value : {:.3f}'.format(model_name, auc(fpr, tpr)))
+
+plt.xlabel('False Positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC Curve')
+plt.legend()
+
+plt.savefig('./deeplearning/data/user_data/images/25_roc_2.png')
+# plt.show()

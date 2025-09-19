965212cabb067f3a3111fa7d5c3ecaa5f5073287
ubuntu_gyuteak <rlarbxor85@gmail.com>
2025-09-19 16:08:24 +0900

    전체 통합 정리

--- deeplearning/python-master/python-master/Django_chart.js/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def main(request):
-    h_loss = [1,2,3,4,5]
-    h_acc = [50, 60, 70, 80]
-    h2_loss = [1,2,3,4,5]
-    h2_acc = [50, 60, 70, 80]
-    return render(request, 'main.html', {'h_loss':h_loss, 'h_acc':h_acc, 'h2_loss':h2_loss, 'h2_acc':h2_acc})
